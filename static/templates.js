5b40f438dc55da8d49392a626af90f7a0e71f050
Kamil Woś <inz.kamil.wos@gmail.com>
2018-10-21 03:22:05 +0200

    Add template js , returns litteral string with rows for each planet, jQuery used

--- static/templates.js
+++ static/templates.js
@@ -1,21 +1,18 @@
-let content = {
+let templates = {
     getRow: function(planet){
             return `  
-            <div class="card board-${board.id}">
+            <div class="">
                 <tr>
-                    <td> {{ planet['name'] }}</td>
-                    <td> {{ planet['diameter'] }} {% if planet['diameter'] != 'unknown' %} KM{%endif%}</td>
-                    <td> {{ planet['climate'] }}</td>
-                    <td> {{ planet['terrain'] }}</td>
-                    <td> {{ planet['surface_water'] }}{% if planet['surface_water'] != 'unknown' %}%{% endif %}</td>
-                    <td> {{ planet['population'] }}{% if planet['population'] != 'unknown' %} people{% endif %}</td>
-                    <td>
-                        {% if (planet['residents'] == []) %}
-                            NO RESIDENTS
-                        {% else %}
-                            RESIDENTS
-                            <!--<button data-url="{{ planet['url'] }}" class="details"> Known residents</button>-->
-                        {% endif %}
+                    <td> ${planet['name']} </td>
+                    <td> ${planet['diameter']}  ${ planet['diameter'] != "unknown" ? ` KM` : ` -- `} </td>
+                    <td> ${planet['climate']} </td>
+                    <td> ${planet['terrain']} </td>
+                    <td> ${planet['surface_water']}  ${ planet['surface_water'] != "unknown" ? ` %` : ` -- `} </td>
+                    
+                    <td> ${planet['population']} ${ planet['population'] != "unknown" ? ` people` : ` -- `} <td>
+                    <td> ${ planet['residents'] == [] ?
+                        ` NO RESIDENTS` : ` <button data-url="${planet['url']}" class="details"> RESIDENTS</button>`} 
+                        
                     </td>
                 </tr>
             </div> `
