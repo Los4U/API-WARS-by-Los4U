829df406edaebf233e347ba613f024e8665c8f01
Kamil Woś <inz.kamil.wos@gmail.com>
2018-11-02 20:53:59 +0100

    Create new route for login and register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 
 
@@ -8,6 +8,52 @@ Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route("/register_user", methods=["POST", "GET"])
+def new_user():
+
+    name = request.form["uname"]
+    psw = request.form["psw"]
+
+    print("name", name, "psw", psw)
+
+    return render_template('register.html')
+
+# @app.route("/register", methods=["POST", "GET"])
+# def register():
+#     #check if login and password is correct!
+#     print(request.form)
+#     login = request.form['login']
+#
+#     password = request.form['password']
+#     id_and_name = data_manager.check_user_login_and_password(login, password)
+#
+#     if id_and_name != []:
+#         login = login.upper()
+#         flash('Username {login} already exists'.format(login=login))
+#
+#         return redirect(url_for('registration'))
+#     else:
+#         # place for function adding user to database
+#         user_id = len(data_manager.get_all_users()) + 1
+#
+#         data_manager.add_user(user_id, login, password)
+#         id_and_name = data_manager.check_user_login_and_password(login, password)
+#         session['username'] = id_and_name[0]['name']
+#         session['id'] = id_and_name[0]['id']
+#
+#         return redirect(url_for('boards') )
 
 
 if __name__ == '__main__':
